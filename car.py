f34bdaa1e644e0c7d56783232d8d3298d6b683ab
kathyayanimarthi <52100041+kathyayanimarthi@users.noreply.github.com>
2023-11-08 21:35:40 -0500

    Update car.py

--- car.py
+++ car.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 
 class Car(ABC):
-    def __init__(self, last_service_date):
+    def __init__(self, last_service_date)
         self.last_service_date = last_service_date
 
     @abstractmethod
